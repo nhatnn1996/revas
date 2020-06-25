3ee70af6f9ba9934fae52c83092546bd14aa7bdd
Nguyen Ngoc Nhat <fimplus@admins-MacBook-Pro-78.local>
2020-06-25 10:21:21 +0700

    [update]

--- src/develop/Music/data.ts
+++ src/develop/Music/data.ts
@@ -7,93 +7,96 @@ export type MusicItemData = {
 
 export const MUSICS: MusicItemData[] = [
   {
-    cover: "https://baoxaydung.com.vn/stores/news_dataimages/hiep/092014/12/10/103333baoxaydung_1.jpg",
+    cover: "//assets.glxplay.io/images/w400/title/flying-tiger_web_posterLandscape_65c9aebee58ca9a80d79f2a62aaec73f.jpg",
+    name: "Chị chị em em",
+    singer: "Hết rồi",
+    color: ["#E97386", "#FF3A5A"],
+  },
+  {
+    cover: "//assets.glxplay.io/images/w400/title/sister-sister_web_posterLandscape_55b67afc9a2e3d5d0f0b185fce7d269a.jpg",
     name: "Tháng năm rực rỡ",
     singer: "黄祺/青猫",
     color: ["#B9C9CF", "#8398A1"],
   },
   {
-    cover: "https://i.ytimg.com/vi/3YQaGTUmJhA/maxresdefault.jpg",
+    cover: "//assets.glxplay.io/images/w400/title/under-the-power_web_posterLandscape_f38d138ab645c15ff72e542e5f0fb43b.jpg",
     name: "Hào quan trở lại",
     singer: "黄祺",
     color: ["#CEE0DF", "#B0BABC"],
   },
   {
-    cover: "https://i.ytimg.com/vi/3YQaGTUmJhA/maxresdefault.jpg",
+    cover: "//assets.glxplay.io/images/w400/title/switched_web_posterLandscape_7daf67aebf852ce7010a287569ca85f5.jpg",
     name: "Chị mười ba",
     singer: "黄祺",
     color: ["#1D1C26", "#21191F"],
   },
   {
-    cover: "https://i.ytimg.com/vi/3YQaGTUmJhA/maxresdefault.jpg",
+    cover: "https://y.gtimg.cn/music/photo_new/T002R800x800M0000038SQcl1z2iC8_1.jpg?max_age=2592000",
     name: "Lan Quế Phường",
     singer: "黄祺",
     color: ["#54626C", "#112430"],
   },
   {
-    cover: "https://i.ytimg.com/vi/3YQaGTUmJhA/maxresdefault.jpg",
+    cover: "//assets.glxplay.io/images/w400/title/scandal-2--hao-quang-tro-lai2_web_posterLandscape_d5893faec88aaa726321c6b95672674e.jpg",
     name: "Tam Thái Tử ",
     singer: "黄祺",
+
     color: ["#555555", "#0B0908"],
   },
   {
-    cover: "https://i.ytimg.com/vi/3YQaGTUmJhA/maxresdefault.jpg",
+    cover: "//assets.glxplay.io/images/w400/title/lan-que-phuong_web_posterLandscape_7a1d079d69e1420110c0850670cbd101.jpg",
     name: "Ai chết giơ tay",
     singer: "黄祺",
+
     color: ["#6B55CE", "#512A69"],
   },
   {
-    cover: "https://baoxaydung.com.vn/stores/news_dataimages/hiep/092014/12/10/103333baoxaydung_1.jpg",
-    name: "Tháng năm rực rỡ",
-    singer: "黄祺/青猫",
-    color: ["#B9C9CF", "#8398A1"],
-  },
-  {
-    cover: "https://i.ytimg.com/vi/3YQaGTUmJhA/maxresdefault.jpg",
-    name: "Hào quan trở lại",
+    cover: "//assets.glxplay.io/images/w400/title/scandal-2--hao-quang-tro-lai2_web_posterLandscape_d5893faec88aaa726321c6b95672674e.jpg",
+    name: "Tam Thái Tử ",
     singer: "黄祺",
-    color: ["#CEE0DF", "#B0BABC"],
+
+    color: ["#555555", "#0B0908"],
   },
   {
-    cover: "https://baoxaydung.com.vn/stores/news_dataimages/hiep/092014/12/10/103333baoxaydung_1.jpg",
-    name: "Tháng năm rực rỡ",
-    singer: "黄祺/青猫",
-    color: ["#B9C9CF", "#8398A1"],
-  },
-  {
-    cover: "https://i.ytimg.com/vi/3YQaGTUmJhA/maxresdefault.jpg",
-    name: "Hào quan trở lại",
+    cover: "//assets.glxplay.io/images/w400/title/lan-que-phuong_web_posterLandscape_7a1d079d69e1420110c0850670cbd101.jpg",
+    name: "Ai chết giơ tay",
     singer: "黄祺",
-    color: ["#CEE0DF", "#B0BABC"],
+
+    color: ["#6B55CE", "#512A69"],
   },
   {
-    cover: "https://baoxaydung.com.vn/stores/news_dataimages/hiep/092014/12/10/103333baoxaydung_1.jpg",
-    name: "Tháng năm rực rỡ",
-    singer: "黄祺/青猫",
-    color: ["#B9C9CF", "#8398A1"],
-  },
-  {
-    cover: "https://i.ytimg.com/vi/3YQaGTUmJhA/maxresdefault.jpg",
-    name: "Hào quan trở lại",
+    cover: "//assets.glxplay.io/images/w400/title/scandal-2--hao-quang-tro-lai2_web_posterLandscape_d5893faec88aaa726321c6b95672674e.jpg",
+    name: "Tam Thái Tử ",
     singer: "黄祺",
-    color: ["#CEE0DF", "#B0BABC"],
+
+    color: ["#555555", "#0B0908"],
   },
   {
-    cover: "https://i.ytimg.com/vi/3YQaGTUmJhA/maxresdefault.jpg",
-    name: "Hào quan trở lại",
+    cover: "//assets.glxplay.io/images/w400/title/lan-que-phuong_web_posterLandscape_7a1d079d69e1420110c0850670cbd101.jpg",
+    name: "Ai chết giơ tay",
     singer: "黄祺",
-    color: ["#CEE0DF", "#B0BABC"],
+
+    color: ["#6B55CE", "#512A69"],
   },
   {
-    cover: "https://i.ytimg.com/vi/3YQaGTUmJhA/maxresdefault.jpg",
-    name: "Hào quan trở lại",
+    cover: "//assets.glxplay.io/images/w400/title/lan-que-phuong_web_posterLandscape_7a1d079d69e1420110c0850670cbd101.jpg",
+    name: "Ai chết giơ tay",
     singer: "黄祺",
-    color: ["#CEE0DF", "#B0BABC"],
+
+    color: ["#6B55CE", "#512A69"],
   },
   {
-    cover: "https://i.ytimg.com/vi/3YQaGTUmJhA/maxresdefault.jpg",
-    name: "Hào quan trở lại",
+    cover: "//assets.glxplay.io/images/w400/title/lan-que-phuong_web_posterLandscape_7a1d079d69e1420110c0850670cbd101.jpg",
+    name: "Ai chết giơ tay",
     singer: "黄祺",
-    color: ["#CEE0DF", "#B0BABC"],
+
+    color: ["#6B55CE", "#512A69"],
+  },
+  {
+    cover: "//assets.glxplay.io/images/w400/title/lan-que-phuong_web_posterLandscape_7a1d079d69e1420110c0850670cbd101.jpg",
+    name: "Ai chết giơ tay",
+    singer: "黄祺",
+
+    color: ["#6B55CE", "#512A69"],
   },
 ];
